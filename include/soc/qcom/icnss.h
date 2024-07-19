3748b50ac6aeefa5f2f4f83ac3fbbf44067568aa
zhuiyight <116001442+zhuiyight@users.noreply.github.com>
2024-07-19 20:04:54 +0800

    Update icnss.h

--- include/soc/qcom/icnss.h
+++ include/soc/qcom/icnss.h
@@ -144,4 +144,5 @@ extern bool icnss_is_fw_ready(void);
 extern bool icnss_is_fw_down(void);
 extern bool icnss_is_rejuvenate(void);
 extern int icnss_trigger_recovery(struct device *dev);
+extern void icnss_block_shutdown(bool status);
 #endif /* _ICNSS_WLAN_H_ */
